7884dd94ae5f3692bc5a258c42e2ed67e5e152cb
Björn Ekengren <bjorn@virtusize.com>
2019-04-09 01:07:27 +0200

    feature: Added support for Swedish language (sv)

--- gruntfile.js
+++ gruntfile.js
@@ -71,7 +71,7 @@ grunt.initConfig({
   },
 
   textbooks: {
-    options: {languages: ['en', 'ru', 'vn'], cache: true},
+    options: {languages: ['en', 'ru', 'vn', 'sv'], cache: false},
     app: {
       files: [{
         expand: true,
